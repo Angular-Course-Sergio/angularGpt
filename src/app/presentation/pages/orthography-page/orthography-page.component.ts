b758337abe15b0c8f8116901b1b94b4b5ca941d8
Sergio Barreras <sergiobg.isc@gmail.com>
2024-08-19 20:41:26 -0700

    Indicador de escritura

--- src/app/presentation/pages/orthography-page/orthography-page.component.ts
+++ src/app/presentation/pages/orthography-page/orthography-page.component.ts
@@ -1,13 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { ChatMessageComponent } from '@components/chat-bubbles/chat-message/chat-message.component';
-import { MyMessageComponent } from '@components/chat-bubbles/my-message/my-message.component';
+import {
+  ChatMessageComponent,
+  MyMessageComponent,
+  TypingLoaderComponent,
+} from '@components/index';
 
 @Component({
   selector: 'app-orthography-page',
   standalone: true,
   templateUrl: './orthography-page.component.html',
-  imports: [CommonModule, ChatMessageComponent, MyMessageComponent],
+  imports: [
+    CommonModule,
+    ChatMessageComponent,
+    MyMessageComponent,
+    TypingLoaderComponent,
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class OrthographyPageComponent {}
