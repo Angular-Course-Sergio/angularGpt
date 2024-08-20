43d6ce5d0e7c14bcdc84376505dbe4eb112d83ec
Sergio Barreras <sergiobg.isc@gmail.com>
2024-08-19 19:52:08 -0700

    Burbujas de chat

--- src/app/presentation/pages/orthography-page/orthography-page.component.ts
+++ src/app/presentation/pages/orthography-page/orthography-page.component.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChatMessageComponent } from '@components/chat-bubbles/chat-message/chat-message.component';
+import { MyMessageComponent } from '@components/chat-bubbles/my-message/my-message.component';
 
 @Component({
   selector: 'app-orthography-page',
   standalone: true,
   templateUrl: './orthography-page.component.html',
-  imports: [CommonModule],
+  imports: [CommonModule, ChatMessageComponent, MyMessageComponent],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class OrthographyPageComponent {}
