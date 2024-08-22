dc8638ec8421d51726be771ef62ec63fcc85d961
Sergio Barreras <sergiobg.isc@gmail.com>
2024-08-22 16:52:50 -0700

    Angular Markdown

--- src/app/presentation/components/text-boxes/text-message-box/text-message-box.component.ts
+++ src/app/presentation/components/text-boxes/text-message-box/text-message-box.component.ts
@@ -32,8 +32,6 @@ export class TextMessageBoxComponent {
     if (this.form.invalid) return;
 
     const { prompt } = this.form.value;
-    console.log(prompt);
-
     this.onMessage.emit(prompt!);
     this.form.reset();
   }
