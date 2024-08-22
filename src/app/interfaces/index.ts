8e3d2811df8ef1e5bfda4cc5f4d9f077dfcf44e4
Sergio Barreras <sergiobg.isc@gmail.com>
2024-08-22 09:30:56 -0700

    Caso de uso - Ortografía

--- src/app/interfaces/index.ts
+++ src/app/interfaces/index.ts
@@ -2,3 +2,4 @@ export * from './message.interface';
 export * from './option.interface';
 export * from './text-message-box-event.interface';
 export * from './text-message-event.interface';
+export * from './orthography-response.interface';
