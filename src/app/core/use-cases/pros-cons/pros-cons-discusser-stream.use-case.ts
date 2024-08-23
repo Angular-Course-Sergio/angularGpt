bcda6704472adc90cec8389b2ea7c3e94b91d410
Sergio Barreras <sergiobg.isc@gmail.com>
2024-08-22 19:43:15 -0700

    Leer respuesta como Stream

--- src/app/core/use-cases/pros-cons/pros-cons-discusser-stream.use-case.ts
+++ src/app/core/use-cases/pros-cons/pros-cons-discusser-stream.use-case.ts
@@ -1,3 +1,36 @@
+import { environment } from 'environments/environment';
+
 export const prosConsDiscusserStreamUseCase = async (prompt: string) => {
-  console.log('prosConsDiscusserStreamUseCase');
+  try {
+    const resp = await fetch(
+      `${environment.backendUri}/pros-cons-discusser-stream`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ prompt }),
+      }
+    );
+
+    if (!resp.ok) throw new Error('No se pudo realizar la comparación');
+
+    const reader = resp.body?.getReader();
+    if (!reader) throw new Error('No se pudo generar el reader');
+
+    const decoder = new TextDecoder();
+    let text = '';
+
+    while (true) {
+      const { value, done } = await reader.read();
+      if (done) break;
+      const decodedChunk = decoder.decode(value, { stream: true });
+      text += decodedChunk;
+      console.log(text);
+    }
+
+    return null;
+  } catch (error) {
+    return null;
+  }
 };
