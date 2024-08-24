af767c1c5e401fd187d12df8a84f0a5c3c94063f
Sergio Barreras <sergiobg.isc@gmail.com>
2024-08-23 21:35:18 -0700

    Realizar la traducción

--- src/app/presentation/pages/translate-page/translate-page.component.ts
+++ src/app/presentation/pages/translate-page/translate-page.component.ts
@@ -1,11 +1,74 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  signal,
+} from '@angular/core';
+import {
+  ChatMessageComponent,
+  MyMessageComponent,
+  TextMessageBoxSelectComponent,
+  TypingLoaderComponent,
+} from '@components/index';
+import { Message } from '@interfaces/message.interface';
+import { OpenAIService } from '@services/openai.service';
+import { Option, TextMessageBoxEvent } from '@interfaces/index';
 
 @Component({
   selector: 'app-translate-page',
   standalone: true,
   templateUrl: './translate-page.component.html',
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ChatMessageComponent,
+    MyMessageComponent,
+    TextMessageBoxSelectComponent,
+    TypingLoaderComponent,
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class TranslatePageComponent {}
+export default class TranslatePageComponent {
+  public openAiService = inject(OpenAIService);
+
+  public messages = signal<Message[]>([]);
+  public isLoading = signal(false);
+
+  public languages = signal<Option[]>([
+    { id: 'alemán', text: 'Alemán' },
+    { id: 'árabe', text: 'Árabe' },
+    { id: 'bengalí', text: 'Bengalí' },
+    { id: 'francés', text: 'Francés' },
+    { id: 'hindi', text: 'Hindi' },
+    { id: 'inglés', text: 'Inglés' },
+    { id: 'japonés', text: 'Japonés' },
+    { id: 'mandarín', text: 'Mandarín' },
+    { id: 'portugués', text: 'Portugués' },
+    { id: 'ruso', text: 'Ruso' },
+  ]);
+
+  handleMessageWithSelect(event: TextMessageBoxEvent) {
+    this.isLoading.set(true);
+    this.messages.update((prev) => [
+      ...prev,
+      {
+        isGpt: false,
+        text: `Traduce al idioma ${event.selectedOption} el siguiente texto: ${event.prompt}`,
+      },
+    ]);
+
+    this.openAiService
+      .translateText(event.prompt, event.selectedOption)
+      .subscribe((resp) => {
+        this.isLoading.set(false);
+
+        this.messages.update((prev) => [
+          ...prev,
+          {
+            isGpt: true,
+            text: resp.message,
+          },
+        ]);
+      });
+  }
+}
