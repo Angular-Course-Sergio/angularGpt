40067f98ef2eec26fa0d9b5e263fa1a2c3600a3f
Sergio Barreras <sergiobg.isc@gmail.com>
2024-08-24 15:09:26 -0700

    Template para transcripciones

--- src/app/presentation/pages/audio-to-text-page/audio-to-text-page.component.ts
+++ src/app/presentation/pages/audio-to-text-page/audio-to-text-page.component.ts
@@ -1,11 +1,39 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  signal,
+} from '@angular/core';
+import { Message, TextMessageEvent } from '@interfaces/index';
+import {
+  ChatMessageComponent,
+  MyMessageComponent,
+  TextMessageBoxFileComponent,
+  TypingLoaderComponent,
+} from '@components/index';
+import { OpenAIService } from '@services/openai.service';
 
 @Component({
   selector: 'app-audio-to-text-page',
   standalone: true,
   templateUrl: './audio-to-text-page.component.html',
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ChatMessageComponent,
+    MyMessageComponent,
+    TypingLoaderComponent,
+    TextMessageBoxFileComponent,
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class AudioToTextPageComponent {}
+export default class AudioToTextPageComponent {
+  public openAiService = inject(OpenAIService);
+
+  public messages = signal<Message[]>([]);
+  public isLoading = signal(false);
+
+  handleMessageWithFile(event: TextMessageEvent) {
+    console.log(event);
+  }
+}
