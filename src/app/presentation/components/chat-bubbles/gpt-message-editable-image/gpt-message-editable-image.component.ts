1c319dca1263f295e03dbd839efa2d350d770327
Sergio Barreras <sergiobg.isc@gmail.com>
2024-08-26 13:37:48 -0700

    Transformar imagen a un canvas

--- src/app/presentation/components/chat-bubbles/gpt-message-editable-image/gpt-message-editable-image.component.ts
+++ src/app/presentation/components/chat-bubbles/gpt-message-editable-image/gpt-message-editable-image.component.ts
@@ -1,4 +1,13 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Output,
+  signal,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-gpt-message-editable-image',
@@ -7,11 +16,32 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   templateUrl: './gpt-message-editable-image.component.html',
   styleUrl: './gpt-message-editable-image.component.css',
 })
-export class GptMessageEditableImageComponent {
+export class GptMessageEditableImageComponent implements AfterViewInit {
+  @ViewChild('canvas') canvasElement?: ElementRef<HTMLCanvasElement>;
+
   @Input({ required: true }) text!: string;
   @Input({ required: true }) imageInfo!: { url: string; alt: string };
 
   @Output() onSelectedImage = new EventEmitter<string>();
+
+  public originalImage = signal<HTMLImageElement | null>(null);
+
+  ngAfterViewInit(): void {
+    if (!this.canvasElement) return;
+
+    const canvas = this.canvasElement.nativeElement;
+    const ctx = canvas.getContext('2d');
+
+    const image = new Image();
+    image.crossOrigin = 'Anonymous';
+    image.src = this.imageInfo.url;
+
+    this.originalImage.set(image);
+
+    image.onload = () => {
+      ctx?.drawImage(image, 0, 0, canvas.width, canvas.height);
+    };
+  }
 
   handleClick() {
     this.onSelectedImage.emit(this.imageInfo.url);
